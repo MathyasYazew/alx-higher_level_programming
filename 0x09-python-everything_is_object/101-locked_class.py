a1acbf516a2fa116c675e95cd74892092e406627
root <mathyasyazew@gmail.com>
2021-12-29 00:44:38 -0800

    Update on 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+    Locked Class Mdule
+    """
+
+
 class LockedClass:
-
-    __slots__ = ['first_name']
-
-    def __init__(self, first_name=''):
-        self.first_name = first_name
+    """
+    prevents the user from dynamically creating new instance attributes,
+    except if the
+    new instance attribute is called first_name.
+    """
+    __slots__ = 'first_name'
