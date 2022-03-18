436471cf89e2469a106d8efd9b0fc5c794ae9acd
MathyasYazew <mathyasyazew@gmail.com>
2022-03-18 08:54:08 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response
+request a body if error display statue code
 """
 
 if __name__ == '__main__':
     import urllib.request
+    import urllib.error
     import sys
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value)
-    data = data.encode("ascii")
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        info = response.read()
-    print(info.decode("ascii"))
+
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            info = response.read()
+            print(info.decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
