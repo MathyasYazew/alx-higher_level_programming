e14539c5ea7c5be2a871ba362afcfbd83603577b
MathyasYazew <88359082+MathyasYazew@users.noreply.github.com>
2022-03-18 18:26:14 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL, sends a request and displays
-the value of the X-Request-Id variable found in the header
-(handling HTTP errors)
-"""
-from urllib import request, error
-import sys
+''' requests package '''
+import requests
 
 
 if __name__ == "__main__":
-        try:
-            with request.urlopen(sys.argv[1]) as response:
-                body = response.read()
-                print(body.decode('utf-8'))
-        except error.HTTPError as err:
-            print('Error code: {}'.format(err.code))
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
